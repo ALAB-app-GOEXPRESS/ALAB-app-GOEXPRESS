2d5b9ddbf43bb41b0ead05fb9dac49f19ca67578
ykurosawa <ykurosawa@jeisryokai.onmicrosoft.com>
2026-02-10 11:31:11 +0900

    初期構築 (#2)
    * Spring Boot Projectを追加
    
    * vite + react-tsプロジェクトの追加
    
    * dockerを利用したビルドに対応
    
    ---------
    
    Co-authored-by: kurosawa.yuta <ykurosawa@inet.jeis.co.jp>

---
+++ backend/src/main/java/com/alab/goexpress/GoexpressApplication.java
@@ -0,0 +1,13 @@
+package com.alab.goexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoexpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoexpressApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/alab/goexpress/GoexpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alab.goexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoexpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
