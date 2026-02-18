13cf92a56baf97f08ac21f95adc1c096df51f9d6
Y.Shimura <shimura.yukinobu.c9h@jp.nssol.nipponsteel.com>
2026-02-18 09:53:32 +0900

    fix: アイコンの色を緑に変更

--- frontend/src/pages/ResultPage/TrainCard.tsx
+++ frontend/src/pages/ResultPage/TrainCard.tsx
@@ -34,7 +34,7 @@ export const TrainCard: React.FC<Props> = ({
       <CardContent className='p-4'>
         <div className='grid grid-cols-[15%_70%_15%] items-center'>
           <div className='flex gap-2'>
-            <TramFront className='bg-black text-white text-[30px] ' />
+            <TramFront className='bg-primary text-white text-[30px] ' />
 
             <div className='flex flex-col'>
               <span className='font-sans text-[20px] font-bold'>{trainTypeName}</span>
