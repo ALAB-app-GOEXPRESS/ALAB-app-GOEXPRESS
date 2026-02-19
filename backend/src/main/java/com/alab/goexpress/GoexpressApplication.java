5ee052136d83ee127dd63053509a0bada48e053d
Y.Shimura <shimura.yukinobu.c9h@jp.nssol.nipponsteel.com>
2026-02-19 10:18:28 +0900

    fix: Mapperアノテーションと鑑賞していたため仮で入れていたMapperScanの指定を削除

--- backend/src/main/java/com/alab/goexpress/GoexpressApplication.java
+++ backend/src/main/java/com/alab/goexpress/GoexpressApplication.java
@@ -1,15 +1,12 @@
 package com.alab.goexpress;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.alab.goexpress.repository")
 public class GoexpressApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GoexpressApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(GoexpressApplication.class, args);
+  }
 }
