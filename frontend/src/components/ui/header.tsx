655145dd645d335206d194d1bc57efd1698eab58
kurosawa.yuta <ykurosawa@jeisryokai.onmicrosoft.com>
2026-02-24 16:50:25 +0900

    両端のパディングを調整、新幹線でさがす、予約確認ナビゲーションを追加

--- frontend/src/components/ui/header.tsx
+++ frontend/src/components/ui/header.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export function Header() {
   return (
-    <header className='sticky top-0 z-50 border-b bg-background/80 backdrop-blur'>
+    <header className='px-15 sticky top-0 z-50 border-b bg-background/80 backdrop-blur'>
       <div className='container flex h-16 items-center justify-between'>
         {/* Left: ロゴ */}
         <Link
@@ -11,6 +11,21 @@ export function Header() {
         >
           <img src='/images/goexpress.png' />
         </Link>
+
+        <nav className='hidden md:flex items-center gap-6 text-sm font-medium'>
+          <Link
+            to='search'
+            className='font-bold text-lg'
+          >
+            新幹線でさがす
+          </Link>
+          <Link
+            to='reservation-list'
+            className='font-bold text-lg'
+          >
+            予約確認
+          </Link>
+        </nav>
       </div>
     </header>
   );
