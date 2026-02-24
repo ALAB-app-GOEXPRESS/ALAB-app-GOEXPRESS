d40b0a1a3f0cc6056845c3da705ad61a90f95ba0
Y.Shimura <shimura.yukinobu.c9h@jp.nssol.nipponsteel.com>
2026-02-24 15:31:39 +0900

    feat: reservation-listをパスに追加

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -3,6 +3,7 @@ import App from './App';
 import { SearchPage } from './pages/SearchPage/SearchPage';
 import { ResultPage } from './pages/ResultPage/ResultPage';
 import { ReservationDetailPage } from './pages/ReservationDetailPage/ReservationDetailPage';
+import { ReservationListPage } from './pages/ReservationListPage/ReservationListPage';
 
 export const router = createBrowserRouter([
   {
@@ -12,6 +13,7 @@ export const router = createBrowserRouter([
       { path: 'search', element: <SearchPage /> },
       { path: 'results', element: <ResultPage /> },
       { path: 'reservation-detail', element: <ReservationDetailPage /> },
+      { path: 'reservation-list', element: <ReservationListPage /> },
     ],
   },
 ]);
