70ec659e30e87eb19fed6a61f3641cad719b78a2
kurosawa.yuta <ykurosawa@jeisryokai.onmicrosoft.com>
2026-02-25 10:00:06 +0900

    現在のページのハイライトにNavLinkに対応

--- frontend/src/components/ui/header.tsx
+++ frontend/src/components/ui/header.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { Ticket, Search } from 'lucide-react';
 
 export function Header() {
@@ -6,28 +6,36 @@ export function Header() {
     <header className='px-15 sticky top-0 z-50 border-b bg-background/80 backdrop-blur'>
       <div className='container flex h-16 items-center justify-between'>
         {/* Left: ロゴ */}
-        <Link
+        <NavLink
           to='results'
           className='font-bold text-lg'
         >
           <img src='/images/goexpress.png' />
-        </Link>
+        </NavLink>
 
         <nav className='hidden md:flex items-center gap-6 text-sm font-medium'>
-          <Link
-            to='search'
-            className='p-2 inline-flex items-center gap-2 font-bold text-lg bg-primary rounded-lg'
+          <NavLink
+            to='results'
+            className={({ isActive }) =>
+              isActive
+                ? 'p-2 inline-flex items-center gap-2 font-bold text-lg rounded-lg bg-primary text-white'
+                : 'p-2 inline-flex items-center gap-2 font-bold text-lg rounded-lg'
+            }
           >
             <Search />
-            新幹線でさがす
-          </Link>
-          <Link
+            新幹線をさがす
+          </NavLink>
+          <NavLink
             to='reservation-list'
-            className='p-2 inline-flex items-center gap-2 font-bold text-lg bg-primary rounded-lg'
+            className={({ isActive }) =>
+              isActive
+                ? 'p-2 inline-flex items-center gap-2 font-bold text-lg rounded-lg bg-primary text-white'
+                : 'p-2 inline-flex items-center gap-2 font-bold text-lg rounded-lg'
+            }
           >
             <Ticket />
             予約確認
-          </Link>
+          </NavLink>
         </nav>
       </div>
     </header>
