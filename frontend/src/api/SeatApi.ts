d0a15308441c056899391bc02e28b6d2df815782
kanzaki.makoto <mkanzaki@inet.jeis.co.jp>
2026-03-05 11:38:46 +0900

    SeatApiを作成

--- frontend/src/api/SeatApi.ts
+++ frontend/src/api/SeatApi.ts
@@ -1,3 +1,29 @@
-export async function FetchSeats() {
-    
+import { fetchJSON } from '@/lib/fetch';
+import { formatSeat } from '@/lib/utils';
+
+export type SeatBetweenApiItem = {
+    seatCd: string;
 }
+
+export type SeatsParams = {
+    trainCd: string;
+    departureDate: string;
+};
+
+export type SeatResult = {
+    convertedSeat: string;
+}
+
+export async function FetchSeats(params: SeatsParams) {
+    const endpoint = `/api/seats?traind_cd=${encodeURIComponent(params.trainCd)}&departure_date=${encodeURIComponent(params.departureDate)}`;
+
+    const data = await fetchJSON<SeatBetweenApiItem[]>(endpoint);
+
+    const converted: SeatResult[] = data.map((seat) => {
+        return {
+            convertedSeat: formatSeat(seat.seatCd)
+        }
+    });
+
+    return converted;
+};
