11c1177b228349cb99935f9b5dff287fd91ae9b6
MakotoKanzaki <mkanzaki@jeisryokai.onmicrosoft.com>
2026-03-05 11:09:19 +0900

    予約済み座席取得API作成 (#58)
    Co-authored-by: kanzaki.makoto <mkanzaki@inet.jeis.co.jp>

--- backend/src/main/java/com/alab/goexpress/seat/SeatMapper.java
+++ backend/src/main/java/com/alab/goexpress/seat/SeatMapper.java
@@ -1,13 +1,14 @@
 package com.alab.goexpress.seat;
 
+import java.time.LocalDate;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface SeatMapper {
-  List<String> reservedSeats (
-    @Param("train_cd") String train_cd,
-    @Param("departure_date") String departure_date
+  List<SeatResponse> findReservedSeats (
+    @Param("trainCd") String trainCd,
+    @Param("departureDate") LocalDate departureDate
   );
 }
