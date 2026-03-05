874654663730319b187796b2e9118aab9b1680f2
kanzaki.makoto <mkanzaki@inet.jeis.co.jp>
2026-03-05 09:53:12 +0900

    Mapperを記述 SeatApi, SeatMapファイル作成（中身はまだ）

--- backend/src/main/java/com/alab/goexpress/seat/SeatMapper.java
+++ backend/src/main/java/com/alab/goexpress/seat/SeatMapper.java
@@ -1,4 +1,13 @@
 package com.alab.goexpress.seat;
 
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
 public interface SeatMapper {
+  List<String> reservedSeats (
+    @Param("train_cd") String train_cd,
+    @Param("departure_date") String departure_date
+  );
 }
