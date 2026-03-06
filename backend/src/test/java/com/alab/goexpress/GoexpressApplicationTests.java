ed2666b703eb3bef2ffc224822e7df6b1b31dabe
Y.Shimura <shimura.yukinobu.c9h@jp.nssol.nipponsteel.com>
2026-03-06 16:46:18 +0900

    fix: テストの依存追加

--- backend/src/test/java/com/alab/goexpress/GoexpressApplicationTests.java
+++ backend/src/test/java/com/alab/goexpress/GoexpressApplicationTests.java
@@ -2,6 +2,12 @@ package com.alab.goexpress;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import com.alab.goexpress.master.train.TrainMasterJpaRepository;
+import com.alab.goexpress.master.plan.PlanMasterJpaRepository;
+import com.alab.goexpress.master.charge.ChargeMasterJpaRepository;
+import com.alab.goexpress.master.train.TrainCarMasterJpaRepository;
+import com.alab.goexpress.seat.SeatReservationJpaRepository;
 
 @SpringBootTest(
   properties = {
@@ -9,6 +15,22 @@ import org.springframework.boot.test.context.SpringBootTest;
   }
 )
 class GoexpressApplicationTests {
+
+  // コンテキスト起動時に必要な依存をモックして、DBやJPA設定なしでも起動可能にする
+  @MockBean
+  TrainMasterJpaRepository trainMasterJpaRepository;
+
+  @MockBean
+  PlanMasterJpaRepository planMasterJpaRepository;
+
+  @MockBean
+  ChargeMasterJpaRepository chargeMasterJpaRepository;
+
+  @MockBean
+  TrainCarMasterJpaRepository trainCarMasterJpaRepository;
+
+  @MockBean
+  SeatReservationJpaRepository seatReservationJpaRepository;
 
   @Test
   void contextLoads() {}
