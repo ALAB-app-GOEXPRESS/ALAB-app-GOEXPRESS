d78e0179e4f42d7a6879c2bfa270edcc671f4241
MakotoKanzaki <mkanzaki@jeisryokai.onmicrosoft.com>
2026-03-11 11:10:05 +0900

    Feature/select seat (#74)
    * コメントアウトしていた座席選択を戻した
    
    * Merge remote-tracking branch 'origin/main' into feature/select-seat
    
    * 座席を選択し、合計金額を算出する機能を作成
    
    * 選択中の座席情報カードを作成
    
    * 選択中の座席情報カードのデザインを作成
    
    * 選択中の座席情報カードに予約するボタンを設置
    
    * 選択中の座席を全件削除するボタンを作成
    
    * ごみ箱ボタンは活性時のみ表示
    座席ボタンは非活性時は通常カーソル
    座席選択の上限を6席に設定
    
    ---------
    
    Co-authored-by: kanzaki.makoto <mkanzaki@inet.jeis.co.jp>

---
+++ frontend/src/pages/SeatMapPage/selectedSeatsInfo.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import type { SelectedSeat } from '@/types/Seat';
+import { formatSeat } from '@/utils/seat';
+import { Badge } from '@/components/ui/badge';
+import { formatCurrency } from '@/utils/currency';
+import { Button } from '@/components/ui/button';
+import { Trash2 } from 'lucide-react';
+
+interface SelectedSeatsInfoProps {
+  selectedSeats: SelectedSeat[];
+  totalPrice: number;
+  handleClickTrash: () => void;
+}
+
+export const SelectedSeatsInfo: React.FC<SelectedSeatsInfoProps> = ({
+  selectedSeats,
+  totalPrice,
+  handleClickTrash,
+}) => {
+  return (
+    <Card className='w-66'>
+      <CardHeader>
+        <CardTitle>選択中の座席</CardTitle>
+        <div className='flex justify-between items-center h-7'>
+          <CardDescription>{selectedSeats.length} 席 選択中</CardDescription>
+          {selectedSeats.length !== 0 && (
+            <button
+              className='cursor-pointer p-1 rounded-md hover:bg-black/5 hover:text-accent-foreground dark:hover:bg-accent/50'
+              onClick={handleClickTrash}
+              disabled={selectedSeats.length === 0}
+            >
+              <Trash2 size={20} />
+            </button>
+          )}
+        </div>
+      </CardHeader>
+      <CardContent className='flex flex-col overflow-y-auto w-65'>
+        {selectedSeats.length > 0 ? (
+          <div className='min-w-max'>
+            <ul className='space-y-2'>
+              {selectedSeats.map(({ carNumber, seatCd, seatTypeName, price }) => (
+                <li
+                  key={`${carNumber}-${seatCd}`}
+                  className='flex justify-between text-sm min-w-max'
+                >
+                  <div className='flex min-w-38 justify-between'>
+                    <span>{formatSeat(seatCd)}</span>
+                    <Badge variant='outline'>{seatTypeName}</Badge>
+                  </div>
+                  <span className='font-bold '>{formatCurrency(price)}</span>
+                </li>
+              ))}
+            </ul>
+            <div className='flex justify-between items-center mt-4'>
+              <span className='font-bold'>合計金額：</span>
+              <span className='font-extrabold text-xl text-primary'>{formatCurrency(totalPrice)}</span>
+            </div>
+          </div>
+        ) : (
+          <p className='text-sm text-muted-foreground'>座席が選択されていません</p>
+        )}
+        <Button
+          className='w-auto mt-4'
+          disabled={selectedSeats.length === 0}
+        >
+          予約する
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
