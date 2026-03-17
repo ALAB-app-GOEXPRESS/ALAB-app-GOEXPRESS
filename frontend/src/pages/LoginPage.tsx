1e48496a0115c9dbf8a28a5b0f25bce521320193
kanzaki.makoto <mkanzaki@inet.jeis.co.jp>
2026-03-17 13:54:31 +0900

    幅を他のページと合わせた

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -6,7 +6,7 @@ export const LoginPage: React.FC = () => {
   }, []);
 
   return (
-    <div className='mt-6'>
+    <div className='mx-auto px-4 sm:px-6 lg:px-8 max-w-screen-2xl pt-6'>
       ログインページにリダイレクトしています。自動で切り替わらない場合は
       <a href='https://ap-northeast-1usoicgi0x.auth.ap-northeast-1.amazoncognito.com/login?client_id=s0ksgqnpm35f0a3jlnkqc329d&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fd84l1y8p4kdic.cloudfront.net'>
         こちら
