b21db3c94ca417f95470244ebc80a3e6e9b6f728
kanzaki.makoto <mkanzaki@inet.jeis.co.jp>
2026-03-18 15:19:29 +0900

    不要なインポートを削除

--- backend/src/main/java/com/alab/goexpress/config/SecurityConfig.java
+++ backend/src/main/java/com/alab/goexpress/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
