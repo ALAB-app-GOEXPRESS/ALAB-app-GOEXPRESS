7eb58c65fd42c500f2fb7a1a9a0be64fd7c55c1f
sunohara <sunohara.tsunohara@jeisryokai.onmicrosoft.com>
2026-03-18 14:17:29 +0900

    fix:コメント位置の修正

--- backend/src/main/java/com/alab/goexpress/train/TrainService.java
+++ backend/src/main/java/com/alab/goexpress/train/TrainService.java
@@ -24,9 +24,9 @@ public class TrainService {
   public List<TrainDto> find(String fromStationCd, String toStationCd, LocalDate date, LocalTime time) {
     List<TrainDto> allTrains = mapper.selectTrainList(fromStationCd, toStationCd);
 
-    // 6時を日付の境界とみなし、それより前の時刻はリストの後方に配置するソート
     final LocalTime boundary = LocalTime.of(6, 0);
 
+    // 6時を日付の境界とみなし、それより前の時刻はリストの後方に配置するソート
     return allTrains.stream()
       .sorted(Comparator.comparing(
           (TrainDto train) -> train.departureTime().isBefore(boundary)
