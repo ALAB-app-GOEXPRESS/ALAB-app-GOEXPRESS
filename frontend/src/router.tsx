2e9ce344e91a43377915d132c385ec653a00558a
kanzaki.makoto <mkanzaki@inet.jeis.co.jp>
2026-03-19 14:08:43 +0900

    ログインページのパスをlogin-page→loginに修正

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -28,7 +28,7 @@ export const router = createBrowserRouter([
       { path: 'reservation-detail', element: <ReservationDetailPage /> },
       { path: 'seat-map', element: <SeatMapPage /> },
       { path: 'reservation-confirm', element: <ReservationConfirmPage /> },
-      { path: 'login-page', element: <LoginPage /> },
+      { path: 'login', element: <LoginPage /> },
       { path: 'login/callback', element: <LoginCallbackPage /> },
 
       // ここから下はログイン必須
