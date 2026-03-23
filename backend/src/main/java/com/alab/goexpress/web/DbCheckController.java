c5ff970f18a69d736ec0b8d97203e1bb3021b7d9
kanzaki.makoto <mkanzaki@inet.jeis.co.jp>
2026-03-23 14:25:30 +0900

    application.propertiesの不要な設定を削除 不要なインポートを削除

--- backend/src/main/java/com/alab/goexpress/web/DbCheckController.java
+++ backend/src/main/java/com/alab/goexpress/web/DbCheckController.java
@@ -1,7 +1,6 @@
 package com.alab.goexpress.web;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
