4ae6dc12344c313beafa55201b28ce3cb9593345
MakotoKanzaki <mkanzaki@jeisryokai.onmicrosoft.com>
2026-03-30 13:18:14 +0900

    ヘッダーのログインボタンから直接cognitoのログインページに飛ぶようにした (#122)
    Co-authored-by: kanzaki.makoto <mkanzaki@inet.jeis.co.jp>

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -74,13 +74,13 @@ export const Header: React.FC = () => {
             </DropdownMenu>
           )}
           {userName === null && (
-            <NavLink
-              to='login'
+            <button
+              onClick={() => window.location.assign('oauth2/authorization/cognito')}
               className='py-1.5 px-3 inline-flex items-center gap-2 text-13 rounded-lg hover:bg-input/50'
             >
               <CircleUser />
               <p>ログイン</p>
-            </NavLink>
+            </button>
           )}
         </nav>
       </div>
